c21477efc5b073a2147aafdd53ee066a59f1ecb4
Yunlong (Yolo) Tang <yunlong.yolo.tang@gmail.com>
2026-04-07 03:04:38 -0700

    fix WeChat

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,5 +2,6 @@ module.exports = {
   content: ["_site/**/*.html", "_site/**/*.js"],
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
+  safelist: ["wechat-modal", "active"],
   skippedContentGlobs: ["_site/assets/**/*.html"],
 };
